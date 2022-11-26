bed6f8871f1882027b8e74eeb8660d27de86d34f
Mitch Parks <mitch.parks101@gmail.com>
2022-11-26 04:35:33 -0600

    Commit package firstProgram and file Test.java

---
+++ JavaParks/src/firstProgram/Test.java
@@ -0,0 +1,11 @@
+package firstProgram;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Testing the commit fuction of Eclipse and GitHub");
+		
+	}
+
+}
